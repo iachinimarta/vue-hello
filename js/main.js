12bbf2a134f6fe30fb670f8e7420dfaaae79338e
Marta Iachini <iachinimarta@gmail.com>
2022-07-22 12:23:14 +0200

    Risolto esercizio

--- js/main.js
+++ js/main.js
@@ -2,11 +2,6 @@ var app = new Vue({
     el: '#app',
     data: {
     message: 'E fino a qui.. ci siamo.',
+    src: "https://picsum.photos/200"
     },
-    methods: {
-        img: function() {
-            let imgBox = document.getElementById('img');
-            imgBox.innerHTML += `<img src="https://picsum.photos/200">`;
-        }
-    }
 });
